488a6c1f3e24486adcedbc5e8fd134ac6d9ee134
AlexYagoFedczuk <alexfed1011@gmail.com>
2024-09-04 10:42:54 -0300

    added Ejercicio 12 and 13

--- clases/Clase_02/Ejercicio_12.php
+++ clases/Clase_02/Ejercicio_12.php
@@ -8,3 +8,33 @@ Realizar el desarrollo de una función que reciba un Array de caracteres y que i
 de las letras del Array.
 Ejemplo: Se recibe la palabra “HOLA” y luego queda “ALOH”.
 */
+
+/**
+ * Invierte el orden de los caracteres en un array.
+ *
+ * Esta función recibe un array de caracteres y retorna un nuevo array
+ * donde los caracteres están en orden inverso.
+ *
+ * @param array $caracteres Array de caracteres a invertir.
+ * @return array Retorna un nuevo array con los caracteres en orden inverso.
+ * @throws InvalidArgumentException Si el parámetro no es un array o si el array contiene elementos no válidos.
+ */
+function invertirArray(array $arrayCaracteres): array {
+    if (!is_array($arrayCaracteres)) {
+        throw new InvalidArgumentException("Error: Se espera un array en el argumento.");
+    }
+
+    foreach ($arrayCaracteres as $caracter) {
+        if (!is_string($caracter) || strlen($caracter) !== 1) {
+            throw new InvalidArgumentException("Error: Cada elemento del array debe ser un caracter.");
+        }
+    }
+
+    return array_reverse($arrayCaracteres);
+}
+
+$arrayOriginal = ['H', 'O', 'L', 'A'];
+$arrayInvertida = invertirArray($arrayOriginal);
+
+echo "Array original:". implode("",$arrayOriginal) ."\n";
+echo "Array invertida:". implode("",$arrayInvertida) ."";
---
+++ clases/Clase_02/Ejercicio_13.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+Aplicación No 13 (Invertir palabra)
+
+Crear una función que reciba como parámetro un string ($palabra) y un entero ($max). La
+función validará que la cantidad de caracteres que tiene $palabra no supere a $max y además
+deberá determinar si ese valor se encuentra dentro del siguiente listado de palabras válidas:
+“Recuperatorio”, “Parcial” y “Programacion”. Los valores de retorno serán: 1 si la palabra
+pertenece a algún elemento del listado.
+0 en caso contrario.
+*/
+
+/**
+ * Verifica si una palabra es válida y no excede el número máximo de caracteres.
+ *
+ * La función recibe una palabra y un valor máximo de caracteres. Valida si la palabra pertenece
+ * a una lista de palabras permitidas y si su longitud no excede el valor máximo.
+ *
+ * @param string $palabra La palabra a verificar.
+ * @param int $max El número máximo permitido de caracteres.
+ * @return int Retorna 1 si la palabra es válida y no excede el número máximo de caracteres, 0 en caso contrario.
+ * @throws InvalidArgumentException Si los argumentos proporcionados no son válidos.
+ */
+function validarStringLen(string $palabra, int $max): int {
+    $retorno = 0;
+
+    if (!is_string($palabra) || !is_int($max)) {
+        throw new InvalidArgumentException("Error: Se espera un 'string' y un 'int' en los argumentos.");
+        //echo "Error: Se espera un 'string' y un 'int' en los argumentos.\n";
+    }
+
+    if (strlen($palabra) > $max) {
+        throw new InvalidArgumentException("Error: La 'Palabra' supera el máximo de caracteres.\n");
+        //echo "Error: La 'Palabra'supera el máximo de caracteres. \n";
+    } else {
+        $palabrasValidas = ["Recuperatorio", "Parcial", "Programacion"];
+        
+        if (in_array($palabra, $palabrasValidas)) {
+            $retorno = 1;
+        }
+    }
+
+    return $retorno;
+}
+
+$palabra1 = 1234; // Parece que PhP transforma automaticamente (si es que puede) el int a string: "1234".
+$max1 = 10;
+echo validarStringLen($palabra1, $max1). "\n";
+
+$palabra2 = "Examen";
+$max2 = 10;
+echo validarStringLen($palabra2, $max2). "\n";
+
+$palabra3 = "Programacion";
+$max3 = 5;
+echo validarStringLen($palabra3, $max3);
