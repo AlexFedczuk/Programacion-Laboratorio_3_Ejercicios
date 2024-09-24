c58b1b0b5217449a0054a3ffd2bec7c362662627
AlexYagoFedczuk <alexfed1011@gmail.com>
2024-09-24 12:22:36 -0300

    Doing the point 3 of the PP for lab.

--- Laboratorio/Primer Parcial/main.js
+++ Laboratorio/Primer Parcial/main.js
@@ -1,22 +1,3 @@
-/*
-import { generarArrayDeObjetos } from "./funtions.js";
-import dataPath from "./data.json"
-
-const arrayDeObjetos = generarArrayDeObjetos(dataPath);
-
-arrayDeObjetos.forEach(obj => {
-    console.log(obj.toString());
-    console.log(obj.toJson());
-});
-
-const empleado = new Empleado(1, "Alex Yago", "Fedczuk", 30, 50000, 120000);
-console.log(empleado.toString());
-console.log(empleado.toJson());
-
-const cliente = new Cliente(2, "Ana", "Isabel de la Cruz", 28, 1500, "123456789");
-console.log(cliente.toString());
-console.log(cliente.toJson());
-*/
 import { generarArrayObjetos } from "./funtions.js";
 
 fetch('./data.json')
